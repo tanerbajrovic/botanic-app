28ff51c7e6ab49b23750300b26c73cc0d1b6cf14
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-03-21 13:02:41 +0100

    Initial commit

---
+++ app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/MainActivity.kt
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rma.tanerbajrovic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
