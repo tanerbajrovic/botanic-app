a52e7906c496c6cccddfd10dc4344dd80c938f49
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-03-31 23:33:09 +0200

    Add dummy spinner implementation

--- app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/MainActivity.kt
@@ -2,10 +2,35 @@ package ba.unsa.etf.rma.tanerbajrovic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var plants: RecyclerView
+    private lateinit var spinner: Spinner
+    private lateinit var adapter: ArrayAdapter<String>
+    private val modes: Array<String> = arrayOf("Medical", "Culinary", "Botanic")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val resetButton: Button = findViewById(R.id.resetBtn)
+        plants = findViewById(R.id.biljkeRV)
+        spinner = findViewById(R.id.modSpinner)
+        adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, modes)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        resetButton.setOnClickListener {
+            spinner.setSelection(0)
+            // Code for RecyclerView adapter
+        }
     }
+
 }
+
+// Clicking a list item changes RecyclerView
+// -> AdapterView for Spinner
+// -> PlantListAdapter
