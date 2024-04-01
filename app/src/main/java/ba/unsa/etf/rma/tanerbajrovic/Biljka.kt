fbde09e0e3803834429d4f3f17392fff63827b30
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-04-01 22:33:55 +0200

    Add corrections to provided classes

--- app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/Biljka.kt
+++ app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/Biljka.kt
@@ -3,7 +3,7 @@ package ba.unsa.etf.rma.tanerbajrovic
 class Biljka(
     val naziv: String,
     val porodica: String,
-    val medicinskoUpozorenja: String,
+    val medicinskoUpozorenje: String,
     val medicinskeKoristi: List<MedicinskaKorist>,
     val profilOkusa: ProfilOkusaBiljke,
     val jela: List<String>,
