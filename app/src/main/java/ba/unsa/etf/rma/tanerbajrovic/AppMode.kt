29eb10a116ef1cc717b89171bc73d50a29c9619b
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-04-02 18:42:17 +0200

    Add SpinnerState

--- app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/AppMode.kt
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.rma.tanerbajrovic
-
-enum class AppMode {
-    MEDICAL,
-    BOTANIC,
-    CULINARY
-}
---
+++ app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/SpinnerState.kt
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rma.tanerbajrovic
+
+enum class AppMode(val description: String) {
+    MEDICAL("Medical mode"),
+    BOTANIC("Botanic mode"),
+    CULINARY("Culinary mode")
+}
