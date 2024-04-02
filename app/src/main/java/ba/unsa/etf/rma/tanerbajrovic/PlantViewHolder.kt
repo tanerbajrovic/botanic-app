81a03bed5e7a34fd3de71716ae2da8513c5f0950
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-04-02 19:32:39 +0200

    Update PlantViewHolder with empty list handling

--- app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/PlantViewHolder.kt
+++ app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/PlantViewHolder.kt
@@ -11,53 +11,68 @@ sealed class PlantViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     val plantName: TextView = itemView.findViewById(R.id.nazivItem)
 
     open fun bind(plant: Biljka) {
+        plantImage.setImageResource(R.mipmap.default_tree)
         plantName.text = plant.naziv
     }
 
     class BotanicPlantViewHolder(itemView: View) : PlantViewHolder(itemView) {
-        val plantFamily: TextView = itemView.findViewById(R.id.porodicaItem)
-        val plantClimate: TextView = itemView.findViewById(R.id.klimatskiTipItem)
-        val plantSoil: TextView = itemView.findViewById(R.id.zemljisniTipItem)
+        private val plantFamily: TextView = itemView.findViewById(R.id.porodicaItem)
+        private val plantClimate: TextView = itemView.findViewById(R.id.klimatskiTipItem)
+        private val plantSoil: TextView = itemView.findViewById(R.id.zemljisniTipItem)
 
         override fun bind(plant: Biljka) {
             super.bind(plant)
             plantFamily.text = plant.porodica
-            plantSoil.text = plant.zemljisniTipovi[0].toString()
-            plantClimate.text = plant.klimatskiTipovi[0].toString()
+            if (plant.zemljisniTipovi.isNotEmpty())
+                plantSoil.text = plant.zemljisniTipovi[0].toString()
+            if (plant.klimatskiTipovi.isNotEmpty())
+                plantClimate.text = plant.klimatskiTipovi[0].toString()
         }
     }
 
     class CulinaryPlantViewHolder(itemView: View) : PlantViewHolder(itemView) {
-        val plantTasteProfile: TextView = itemView.findViewById(R.id.profilOkusa)
-        val plantDishOne: TextView = itemView.findViewById(R.id.jelo1Item)
-        val plantDishTwo: TextView = itemView.findViewById(R.id.jelo2Item)
-        val plantDishThree: TextView = itemView.findViewById(R.id.jelo3Item)
+        private val plantTasteProfile: TextView = itemView.findViewById(R.id.profilOkusa)
+        private val plantDishOne: TextView = itemView.findViewById(R.id.jelo1Item)
+        private val plantDishTwo: TextView = itemView.findViewById(R.id.jelo2Item)
+        private val plantDishThree: TextView = itemView.findViewById(R.id.jelo3Item)
 
         override fun bind(plant: Biljka) {
             plantTasteProfile.text = plant.profilOkusa.toString()
-//                culinaryHolder.plantDishOne.text = plants[position].jela[0]
-//                culinaryHolder.plantDishTwo.text = plants[position].jela[1]
-//                culinaryHolder.plantDishThree.text = plants[position].jela[2]
-            plantDishOne.text = "RandomOne"
-            plantDishTwo.text = "RandomTwo"
-            plantDishThree.text = "RandomThree"
+            if (plant.jela.size >= 3) { // There's a better way to do this, probably.
+                plantDishOne.text = plant.jela[0]
+                plantDishTwo.text = plant.jela[1]
+                plantDishThree.text = plant.jela[2]
+            }
+            else if (plant.jela.size == 2) {
+                plantDishOne.text = plant.jela[0]
+                plantDishTwo.text = plant.jela[1]
+            }
+            else if (plant.jela.size == 1) {
+                plantDishOne.text = plant.jela[0]
+            }
         }
     }
 
     class MedicalPlantViewHolder(itemView: View) : PlantViewHolder(itemView) {
-        val plantWarning: TextView = itemView.findViewById(R.id.upozorenjeItem)
-        val plantRemedyOne: TextView = itemView.findViewById(R.id.korist1Item)
-        val plantRemedyTwo: TextView = itemView.findViewById(R.id.korist2Item)
-        val plantRemedyThree: TextView = itemView.findViewById(R.id.korist3Item)
+        private val plantWarning: TextView = itemView.findViewById(R.id.upozorenjeItem)
+        private val plantRemedyOne: TextView = itemView.findViewById(R.id.korist1Item)
+        private val plantRemedyTwo: TextView = itemView.findViewById(R.id.korist2Item)
+        private val plantRemedyThree: TextView = itemView.findViewById(R.id.korist3Item)
 
         override fun bind(plant: Biljka) {
             plantWarning.text = plant.medicinskoUpozorenje
-//                medicalHolder.plantRemedyOne.text = plants[position].medicinskeKoristi[0].toString()
-//                medicalHolder.plantRemedyTwo.text = plants[position].medicinskeKoristi[1].toString()
-//                medicalHolder.plantRemedyThree.text = plants[position].medicinskeKoristi[2].toString()
-            plantRemedyOne.text = "RandomOne"
-            plantRemedyTwo.text = "RandomTwo"
-            plantRemedyThree.text = "RandomThree"
+            if (plant.medicinskeKoristi.size >= 3) { // There's a better way to do this, probably.
+                plantRemedyOne.text = plant.medicinskeKoristi[0].toString()
+                plantRemedyTwo.text = plant.medicinskeKoristi[1].toString()
+                plantRemedyThree.text = plant.medicinskeKoristi[2].toString()
+            }
+            else if (plant.jela.size == 2) {
+                plantRemedyOne.text = plant.medicinskeKoristi[0].toString()
+                plantRemedyTwo.text = plant.medicinskeKoristi[1].toString()
+            }
+            else if (plant.jela.size == 1) {
+                plantRemedyOne.text = plant.medicinskeKoristi[0].toString()
+            }
         }
     }
 
