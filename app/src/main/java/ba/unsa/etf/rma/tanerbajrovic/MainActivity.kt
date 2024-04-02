81584bdf3d5d46c423df4eb7d42f441f50e0a46a
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-04-02 21:40:06 +0200

    Add ability to change views through spinner

--- app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/MainActivity.kt
@@ -2,6 +2,8 @@ package ba.unsa.etf.rma.tanerbajrovic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var plantsAdapter: PlantListAdapter
     private lateinit var spinner: Spinner
     private lateinit var spinnerAdapter: ArrayAdapter<String>
-    private val modes: Array<String> = arrayOf("Medical", "Culinary", "Botanic")
+    private val states: Array<String> = arrayOf("Medical", "Culinary", "Botanic")
     private var listOfPlants: List<Biljka> = getPlants()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,15 +34,37 @@ class MainActivity : AppCompatActivity() {
         plants.adapter = plantsAdapter
         plantsAdapter.updatePlants(listOfPlants)
         spinner = findViewById(R.id.modSpinner)
-        spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, modes)
+        spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, states)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = spinnerAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val newItem: String = states[p2]
+                spinnerState = when (newItem) {
+                    states[0] -> {
+                        SpinnerState.MEDICAL
+                    }
+                    states[1] -> {
+                        SpinnerState.CULINARY
+                    }
+                    states[2] -> {
+                        SpinnerState.BOTANIC
+                    }
+                    else -> {
+                        throw Exception("Invalid state")
+                    }
+                }
+                plantsAdapter.changeSpinnerState(spinnerState)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                spinner.setSelection(0)
+                spinnerState = SpinnerState.MEDICAL
+            }
+        }
         resetButton.setOnClickListener {
             spinner.setSelection(0)
-            spinnerState = SpinnerState.MEDICAL
-            plantsAdapter.changeSpinnerState(spinnerState)
         }
-
     }
 
 }
