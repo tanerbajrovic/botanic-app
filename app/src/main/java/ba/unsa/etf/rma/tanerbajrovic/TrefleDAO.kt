8bb34c3f1234859fb3b2f00332952c895ccd1876
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-06-05 17:30:04 +0200

    Add unit tests and dummy implementation of TrefleDAO

---
+++ app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/TrefleDAO.kt
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rma.tanerbajrovic
+
+import android.graphics.Bitmap
+
+class TrefleDAO {
+
+    private val trefle_api_key: String = "c1haNvSntbc7uC9wF_JYJDThU__-jscC0AO9iy8G17w"
+    // private val defaultBitmap: Bitmap = Bitmap.createBitmap()
+
+    fun getImage(plant: Biljka): Biljka {
+        TODO()
+    }
+
+    fun fixData(plant: Biljka): Biljka {
+        TODO()
+    }
+
+    fun getPlantsWithFlowerColor(flowerColor: String, substring: String): List<Biljka> {
+        TODO()
+    }
+
+}
