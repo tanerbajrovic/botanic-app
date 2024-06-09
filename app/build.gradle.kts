17c0c87f5df512f1cd9005bc198d943d3b87f806
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-06-09 15:54:03 +0200

    Update dependencies and add API keys

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.initialization.Environment
+import org.jetbrains.kotlin.konan.properties.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -7,6 +10,9 @@ android {
     namespace = "ba.unsa.etf.rma.tanerbajrovic"
     compileSdk = 34
 
+    buildFeatures {
+        buildConfig = true
+    }
     defaultConfig {
         applicationId = "ba.unsa.etf.rma.tanerbajrovic"
         minSdk = 30
@@ -15,8 +21,12 @@ android {
         versionName = "1.0"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        val keyStoreFile = project.rootProject.file("apikeys.properties")
+        val properties = Properties()
+        properties.load(keyStoreFile.inputStream())
+        val trefleApiKey = properties.getProperty("TREFLE_API_KEY") ?: ""
+        buildConfigField("String", "TREFLE_API_KEY", trefleApiKey)
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -37,12 +47,21 @@ android {
 
 dependencies {
 
+    // Android stuff
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    // APIs
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    // Image loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    // Testing
     testImplementation("junit:junit:4.13.2")
     testImplementation ("org.assertj:assertj-core:3.22.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
