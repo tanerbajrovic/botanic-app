3858cf775ffc6d1189f4bb151e36c5a0dd525e50
Taner Bajrovic <tanerbajrovic@gmail.com>
2024-06-22 01:04:31 +0200

    Fix typo in values/strings

--- app/src/main/java/ba/unsa/etf/rma/tanerbajrovic/api/model/FamilyResponse.kt
+++
@@ -1,8 +0,0 @@
-package ba.unsa.etf.rma.tanerbajrovic.api.model
-
-import com.google.gson.annotations.SerializedName
-
-data class FamilyResponse(
-    @SerializedName("id") val id: Long,
-    @SerializedName("name") val latinName: String?
-)
